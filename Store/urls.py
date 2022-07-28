b9717c0e3147c7e1f948591b835751f06fa79126
henrique.beliato <rick_beliato@yahoo.com.br>
2022-07-27 22:26:49 -0300

    Inserir dados HTML

--- Store/urls.py
+++ Store/urls.py
@@ -1,7 +1,11 @@
+from unicodedata import name
 from django.urls import path
 from Store import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('teste/', views.teste, name='teste')
+    path('teste/', views.teste, name='teste'),
+    path('departamentos/', views.departamentos, name='departamentos'),
+    path('categorias/', views.categorias, name='categorias'),
+    path('produtos/', views.produtos, name='produtos')
 ]
--- Store/views.py
+++ Store/views.py
@@ -2,6 +2,9 @@ from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 from Store.models import Departamento
+from Store.models import Categoria
+from Store.models import Produto
+
 
 # Create your views here.
 def index(request):
@@ -16,3 +19,17 @@ def teste(request):
     context = {'departamentos': dpto}
     return render(request, 'teste.html', context)
 
+def departamentos(request):
+    dpto = Departamento.objects.all()
+    context = {'departamentos': dpto}
+    return render(request, 'departamentos.html', context)
+
+def categorias(request):
+    lista_categorias = Categoria.objects.all()
+    context = {'categorias': lista_categorias}
+    return render(request, 'categorias.html', context)
+
+def produtos(request):
+    relacao_produtos = Produto.objects.all()
+    context = {'Produtos': relacao_produtos}
+    return render(request, 'Produtos.html', context)
