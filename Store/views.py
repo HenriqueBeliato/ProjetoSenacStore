5ba23b9ad60e669c9c016c30ef427b9cf55ac608
henrique.beliato <rick_beliato@yahoo.com.br>
2022-08-03 22:14:37 -0300

    Nova Atualização

--- Store/views.py
+++ Store/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from requests import request
 from Store.models import Departamento
 from Store.models import Categoria
 from Store.models import Produto
@@ -13,23 +14,62 @@ def index(request):
     context = {'nome': meu_nome, 'artigo': 'o' if sexo =='M' else 'a'}
     return render(request, 'index.html', context)
 
+
 def teste(request):
     # dpto = ['Casa', 'Informática', 'Telefonia', 'Internet', 'Games']
     dpto = Departamento.objects.all()
     context = {'departamentos': dpto}
     return render(request, 'teste.html', context)
 
+
+
 def departamentos(request):
     dpto = Departamento.objects.all()
     context = {'departamentos': dpto}
     return render(request, 'departamentos.html', context)
 
+
+
+def lista_categ(request):
+    lista_categorias = Categoria.objects.all()
+    context = {'categorias': lista_categorias}
+    return render(request, 'categorias.html', context)
+
+
+
 def categorias(request, id):
     lista_categorias = Categoria.objects.filter(departamento_id = id)
-    context = {'categorias': lista_categorias}
+    depto = Departamento.objects.get(id = id)
+    context = {
+        'categorias': lista_categorias,
+        'departamento': depto
+        }
     return render(request, 'categorias.html', context) 
+
+
+
+def lista_prod(request):
+    relacao_produtos = Produto.objects.all()
+    context = {'categorias': relacao_produtos}
+    return render(request, 'Produtos.html', context)
+
+
 
 def produtos(request, id):
     relacao_produtos = Produto.objects.filter(categoria_id = id)
-    context = {'Produtos': relacao_produtos}
+    prod = Categoria.objects.get(id = id)
+    context = {
+        'Produtos': relacao_produtos,
+        'Categoria': prod
+        }
     return render(request, 'Produtos.html', context)
+
+
+
+def produto_detalhe(request, id):
+    det_prod = Produto.objects.get(id = id)
+    context = {
+        'Produto_Detalhe': det_prod,
+        'Produto': det_prod
+        }
+    return render(request, 'produto_detalhe.html', context)
