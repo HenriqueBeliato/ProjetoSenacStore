13dc2fdd0775f70498887d57169e1aaf25e56797
henrique.beliato <rick_beliato@yahoo.com.br>
2022-08-10 22:14:51 -0300

    Atualização

--- ProjetoSenacStore/settings.py
+++ ProjetoSenacStore/settings.py
@@ -125,3 +125,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+#config e-mail GMAIL - Dados de SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'lickbeliato@gmail.com'
+EMAIL_HOST_PASSWORD = 'gtfihybzvfrddkzk'
